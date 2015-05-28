6c9e3b9f0c9718ed3358d5955abe27f146466364
saidhdr <saidhdr@gmail.com>
2015-05-28 21:34:46 +0800

    First Solution

---
+++ Librarian/Librarian/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Librarian.Startup))]
+namespace Librarian
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
